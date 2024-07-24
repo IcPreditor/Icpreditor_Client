913478907082dbd352b1b69ff5eafd9a882c8bcf
GuilhermeFernandesCC <guilherme.fernandes@ccc.ufcg.edu.br>
2024-07-24 20:43:56 -0300

    Contagem de Desconhecidos

--- testes/testeRho.py
+++ testes/testeRho.py
@@ -15,32 +15,31 @@ else:
 print(output)
 
 
-while(True):
-    for rb_int in range(1,10,2):
-        rb = rb_int/10.0
-        for ra_int in range(1,10,2):
-            ra = ra_int/10.0
-            ArtMap = ARTMAPFUZZY(input, output, rhoARTa=ra, rhoARTb=rb)
-            ArtMap.train()
+for rb_int in range(1,10,2):
+    rb = rb_int/10.0
+    for ra_int in range(1,10,2):
+        ra = ra_int/10.0
+        ArtMap = ARTMAPFUZZY(input, output, rhoARTa=ra, rhoARTb=rb)
+        ArtMap.train()
 
 
 
-            n = len(input)
+        n = len(input)
 
-            sucesso = 0
-            for y in range(len(input)):
-                resultado = ArtMap.test(input[y]).get("index")
-                '''
-                print("")
-                print("input: " + str(input[y]))
-                print("output: " + str(output[y]))
-                print("ArtB:" + str(resultado)) #{'index': 0, 'ArtB': [0.0, 1.0], 'id': '0010'}
-                print("")
-                '''
+        sucesso = 0
+        for y in range(len(input)):
+            resultado = ArtMap.test(input[y]).get("index")
+            '''
+            print("")
+            print("input: " + str(input[y]))
+            print("output: " + str(output[y]))
+            print("ArtB:" + str(resultado)) #{'index': 0, 'ArtB': [0.0, 1.0], 'id': '0010'}
+            print("")
+            '''
 
-                if (output[y]==aux3[resultado]):
-                    sucesso += 1
-            print("RhoA: " + str(ra))
-            print("RhoB: " + str(rb))
-            print(str(sucesso)+"/"+str(n))
-            print(sucesso/n)
+            if (output[y]==aux3[resultado]):
+                sucesso += 1
+        print("RhoA: " + str(ra))
+        print("RhoB: " + str(rb))
+        print(str(sucesso)+"/"+str(n))
+        print(sucesso/n)
