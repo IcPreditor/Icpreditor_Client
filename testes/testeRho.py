5b19aff11de9d6acbcbb1e3482fa9cfcb6a19bb1
GuilhermeFernandesCC <guilherme.fernandes@ccc.ufcg.edu.br>
2024-08-09 16:34:21 -0300

    Refactor Treino/Teste

--- testes/testeRho.py
+++ testes/testeRho.py
@@ -8,11 +8,20 @@ sys.path.insert(1, r'C:\Users\m5253\VisualStudio\Icpreditor\processing')
 import processing
 
 #input,output = bf.bancoFalso(2000)
-input,output = processing.getInputOutput()
-input = [list(item[0]) for item in input]
-input = np.array(input)
+students_in,students_out = processing.getInputOutput()
+students_in = [list(item[0]) for item in students_in]
+i = len(students_in)
+##Train
+input = np.array(students_in[0:i//2])
 input = input.astype(int)
-output = np.array(output)
+output = np.array(students_out[0:i//2])
+
+
+##Teste
+teste_in = np.array(students_in[i//2:i-1])
+teste_in = teste_in.astype(int)
+teste_out = np.array(students_out[i//2:i-1])
+
 
 if output[0][0]==0:
     #print(output[0][0])
@@ -27,14 +36,21 @@ for rb_int in range(1,10,2):
     rb = rb_int/10.0
     for ra_int in range(1,10,2):
         ra = ra_int/10.0
+        print("##Iniciando Treinamento##")
+        print("RhoA: " + str(ra))
+        print("RhoB: " + str(rb))
         ArtMap = ARTMAPFUZZY(input, output, rhoARTa=ra, rhoARTb=rb)
         ArtMap.train()
+        print("##Treinamento Finalizado##")
 
-        n = len(input)
-
+        #print(teste_in)
+        #print(teste_out)
+        n = len(teste_in)
         sucesso = 0
-        for y in range(len(input)):
-            resultado = ArtMap.test(input[y]).get("index")
+
+        print("##Iniciando Teste##")
+        for y in range(n):
+            resultado = ArtMap.test(teste_in[y]).get("index")
             '''
             print("")
             print("input: " + str(input[y]))
@@ -42,11 +58,10 @@ for rb_int in range(1,10,2):
             print("ArtB:" + str(resultado)) #{'index': 0, 'ArtB': [0.0, 1.0], 'id': '0010'}
             print("")
             '''
-
-            if (output[y]==aux3[resultado]):
+            if (teste_out[y]==aux3[resultado]):
                 sucesso += 1
 
-        print("RhoA: " + str(ra))
-        print("RhoB: " + str(rb))
+        print("##Teste Finalizado##")
+        print("Resultados:")
         print(str(sucesso)+"/"+str(n))
         print(sucesso/n)
