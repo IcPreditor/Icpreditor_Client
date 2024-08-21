01f13d134bbd0a9bbee3bd22639b3f3ab1067969
eduardoaugustoabc <eduardoaugustoabc@gmail.com>
2024-08-21 14:44:16 -0300

    padronizacao num bits, cabecalho e comentarios

--- processing/processing.py
+++ processing/processing.py
@@ -1,23 +1,15 @@
+#Data processing, mapping and transformation on the bases we use
+#Eduardo Augusto, 2024
+
 import json
 import pandas as pd
 import numpy as np
 from dicTurno import getTurno
 
-#    "gender": {"MASCULINO": "00", "FEMININO": "01", "OUTRO": "10", "DESCONHECIDO": "11"},
-#    "nationality": {"BRASILEIRA": "0", "ESTRANGEIRA": "1"},
-#    "maritalStatus": {"SOLTEIRO": "000", "CASADO": "001", "SEPARADO": "010", "VIUVO": "011", "DIVORCIADO": "100", "DESCONHECIDO": "101"},
-#    "status": {"GRADUADO": "00", "ATIVO": "01", "INATIVO": "10"},
-#    "inactivityReason": {"ABANDONO": "000", "DESCONHECIDO": "001", "TRANSFERENCIA": "010", "CONCLUIU_MAS_NAO_COLOU_GRAU": "011", "DESISTENCIA": "100"},
-#    "affirmativePolicy": {"A0": "0000", "L1": "0001", "L2": "0010", "L5": "0011", "L6": "0100", "L9": "0101", "L10": "0110", "L13": "0111", "L14": "1000", "BONUS": "1001"},
-#    "secondarySchoolType": {"PRIVADA": "000", "PUBLICA": "001", "MAJORITARIAMENTE_PUBLICA": "010", "MAJORITARIAMENTE_PRIVADA": "011", "DESCONHECIDA": "100"}
-#    "courseCode" : {"M":'00', "V":'01', "N":'10', "D":'11'}
-
-
-
 # Carrega dicionario com codeCurso/turno
-dicTurnos = getTurno()
+dictionaryTurnos = getTurno()
 def whatTurno(row):
-    return dicTurnos[str(row["courseCode"])]
+    return dictionaryTurnos[str(row["courseCode"])]
 
 def load_large_json(file_path):
     return pd.read_json(file_path)
@@ -29,19 +21,19 @@ file_path = r'C:\Users\Usuário\Icpreditor\data\students.json'
 dataframe = load_large_json(file_path)
     
 # Obter dados dos estudantes
-#df = get_student_data()
+
 #Fazer lista de turnos
 dataframe["turno"] = dataframe.apply(lambda row: getTurno()  , axis=1)
 
 # Mapeamento binário para valores categóricos
 binary_mappings = {
-    "gender": {"MASCULINO": "00", "FEMININO": "01", "OUTRO": "10", "DESCONHECIDO": "11"},
-    "nationality": {"BRASILEIRA": "00", "ESTRANGEIRA": "01"},
-    "maritalStatus": {"SOLTEIRO": "000", "CASADO": "001", "SEPARADO": "010", "VIUVO": "011", "DIVORCIADO": "100", "DESCONHECIDO": "101"},
-    "status": {"GRADUADO": "00", "ATIVO": "01", "INATIVO": "10"},
-    "inactivityReason": {"ABANDONO": "000", "DESCONHECIDO": "001", "TRANSFERENCIA": "010", "CONCLUIU_MAS_NAO_COLOU_GRAU": "011", "DESISTENCIA": "100"},
-    "affirmativePolicy": {"A0": "000", "L1": "001", "L2": "010", "L5": "011", "L6": "100", "L9": "101", "L10": "110", "L13": "111", "L14": "1000", "BONUS": "1001"},
-    "secondarySchoolType": {"PRIVADA": "00", "PUBLICA": "01", "MAJORITARIAMENTE_PUBLICA": "10", "MAJORITARIAMENTE_PRIVADA": "11", "DESCONHECIDA": "100"}
+    "gender": {"MASCULINO": "0000", "FEMININO": "0001", "OUTRO": "0010", "DESCONHECIDO": "0011"},
+    "nationality": {"BRASILEIRA": "0000", "ESTRANGEIRA": "0001"},
+    "maritalStatus": {"SOLTEIRO": "0000", "CASADO": "0001", "SEPARADO": "0010", "VIUVO": "0011", "DIVORCIADO": "0100", "DESCONHECIDO": "0101"},
+    "status": {"GRADUADO": "0000", "ATIVO": "0001", "INATIVO": "0010"},
+    "inactivityReason": {"ABANDONO": "0000", "DESCONHECIDO": "0001", "TRANSFERENCIA": "0010", "CONCLUIU_MAS_NAO_COLOU_GRAU": "0011", "DESISTENCIA": "0100"},
+    "affirmativePolicy": {"A0": "0000", "L1": "0001", "L2": "0010", "L5": "0011", "L6": "0100", "L9": "0101", "L10": "0110", "L13": "0111", "L14": "1000", "BONUS": "1001"},
+    "secondarySchoolType": {"PRIVADA": "0000", "PUBLICA": "0001", "MAJORITARIAMENTE_PUBLICA": "0010", "MAJORITARIAMENTE_PRIVADA": "0011", "DESCONHECIDA": "0100"}
 }
 
 #Ajuste do secondarySchoolType desconhecido
@@ -52,38 +44,48 @@ def adjust_secondary_school_type(row):
         else:
             return "PUBLICA"
     return row["secondarySchoolType"]
+
 def getInputOutput():
     # Caminho para o arquivo JSON
     file_path = r'data/students.json'
+    
+    #Carregar os dados
+    dataframe = load_large_json(file_path)
 
-dataframe["secondarySchoolType"] = dataframe.apply(adjust_secondary_school_type, axis=1)
+    dataframe["secondarySchoolType"] = dataframe.apply(adjust_secondary_school_type, axis=1)
 
-# Converter valores categóricos para sua representação binária
-for col, mapping in binary_mappings.items():
-    dataframe[col] = dataframe[col].map(mapping)
-# Converter valores numéricos para binário
-dataframe["age"] = dataframe["age"].apply(lambda x: format(int(x), '08b') if pd.notnull(x) else "0"*8)
-dataframe["courseCode"] = dataframe["courseCode"].apply(lambda x: format(int(x), '020b') if pd.notnull(x) else "0"*20)
-#df["curriculumCode"] = df["curriculumCode"].apply(lambda x: ''.join(format(ord(i), '08b') for i in x) if pd.notnull(x) else "0"*32)
-dataframe["curriculumCode"] = dataframe["curriculumCode"].apply(
-    lambda x: ''.join(format(ord(i), '08b') for i in str(x)) if pd.notnull(x) else "0"*32
-)
+    # Converter valores categóricos para sua representação binária
+    for column, mapping in binary_mappings.items():
+        dataframe[column] = dataframe[column].map(mapping)
 
-#Fazer uma lista dos estudantes que evadiram com base na sua razao de inatividade
-dataframe['evaded'] = dataframe.apply(lambda row: '1' if row['status'] == '10' and row['inactivityReason'] != '010' else '0', axis=1)
-evaded_list = dataframe['evaded'].tolist()
-# Colunas a serem mantidas
-columns_to_keep = ["age", "gender", "nationality", "maritalStatus", "affirmativePolicy", "secondarySchoolType", "courseCode", "curriculumCode"]
+    # Converter valores numéricos para binário
+    dataframe["age"] = dataframe["age"].apply(lambda x: format(int(x), '08b') if pd.notnull(x) else "0"*8)
+    dataframe["courseCode"] = dataframe["courseCode"].apply(lambda x: format(int(x), '020b') if pd.notnull(x) else "0"*20)
 
-# Remover todas as colunas que não estão em columns_to_keep
-dataframe = dataframe.drop(columns=[column for column in dataframe.columns if column not in columns_to_keep])
-# Substituir NaN por binário de 0s
-dataframe.fillna("0", inplace=True)
-dataframe = dataframe.astype(str)
+    #dataframe["curriculumCode"] = dataframe["curriculumCode"].apply(lambda x: ''.join(format(ord(i), '08b') for i in x) if pd.notnull(x) else "0"*32)
+    dataframe["curriculumCode"] = dataframe["curriculumCode"].apply(
+        lambda x: ''.join(format(ord(i), '08b') for i in str(x)) if pd.notnull(x) else "0"*32
+    )
 
-#Concatenar os resultados em uma string binária por linha
-binary_strings = dataframe.apply(lambda row: "".join(row.values), axis=1).to_list()
-print(binary_strings)
+    #Fazer uma lista dos estudantes que evadiram com base na sua razao de inatividade
+    dataframe['evaded'] = dataframe.apply(lambda row: '1' if row['status'] == '10' and row['inactivityReason'] != '010' else '0', axis=1)
+    evaded_list = dataframe['evaded'].tolist()
 
-# Visualizar o resultado
-print(evaded_list)
+    # Colunas a serem mantidas
+    columns_to_keep = ["age", "gender", "nationality", "maritalStatus", "affirmativePolicy", "secondarySchoolType", "courseCode", "curriculumCode"]
+
+    # Remover todas as colunas que não estão em columns_to_keep
+    dataframe = dataframe.drop(columns=[column for column in dataframe.columns if column not in columns_to_keep])
+
+    # Substituir NaN por binário de 0s
+    dataframe.fillna("0", inplace=True)
+    dataframe = dataframe.astype(str)
+
+    #Concatenar os resultados em uma string binária por linha
+    binary_strings = dataframe.apply(lambda row: "".join(row.values), axis=1).to_list()
+    print(binary_strings)
+
+    # Visualizar o resultado
+    print(evaded_list)
+
+getInputOutput()
