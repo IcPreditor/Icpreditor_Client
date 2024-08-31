8968a9be4e34330d225e394217854300b4cd4bb2
GuilhermeFernandesCC <guilherme.fernandes@ccc.ufcg.edu.br>
2024-08-31 11:31:35 -0300

    Update API Eureca V2

--- dataGenerator/genStudents.py
+++ dataGenerator/genStudents.py
@@ -10,8 +10,8 @@ import json
 
 
 url_token = 'https://pre.ufcg.edu.br:8443/as_scao'
-url_eureca = 'https://eureca.sti.ufcg.edu.br/das'
-
+#url_eureca = 'https://eureca.sti.ufcg.edu.br/das'
+url_eureca= 'https://eureca.sti.ufcg.edu.br/das/v2'
 #request token
 def genToken(data):
     request = req.post((url_token+"/as/tokens"), json=data )
@@ -31,7 +31,7 @@ def getProfile(token):
 #request Courses Actives (using token)
 def saveCoursesActives(token):
     headers = {'content-type':'application/json',"authentication-token":token}
-    request = req.get(url_eureca+"/courses/getActives",headers=headers)
+    request = req.get(url_eureca+"/cursos?status-enum=ATIVOS&campus=1",headers=headers)
     #save coursesActives
     with open("data/coursesActives.json","w") as courses_file:
             json.dump(request.json(),courses_file)
@@ -40,12 +40,9 @@ def saveCoursesActives(token):
 #student by course // 2017.1-2023.2
 def saveStudents(token):
     headers = {'content-type':'application/json',"authentication-token":token}
-    students = []
-    for course in json.load(open("data/coursesActives.json")):
-        request = req.get(url_eureca+"/students?courseCode="+course["code"]+"&from=2023.1&to=2023.2&anonymize=true",headers=headers)
-        students.extend(request.json()["students"])
+    request = req.get(url_eureca+"/estudantes?periodo-de-ingresso-de=2023.1&periodo-de-ingresso-ate=2023.2&campus=1",headers=headers)
     with open("data/students.json","w") as students_file:
-        json.dump(students,students_file)
+        json.dump(request.json(),students_file)
 
 #For test
 
@@ -64,6 +61,6 @@ credentials.close()
 #get profile
 print(getProfile(token))
 #get courses
-print(saveCoursesActives(token))
+#print(saveCoursesActives(token))
 #get students 2017
 print(saveStudents(token))
