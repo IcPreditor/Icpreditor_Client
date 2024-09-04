f8d4dbd6471bab405e3f798fa1a721ab73518c51
eduardoaugustoabc <eduardoaugustoabc@gmail.com>
2024-09-04 13:15:25 -0300

    mapeamento das novas variaveis corrigidos

--- processing/processing.py
+++ processing/processing.py
@@ -25,16 +25,16 @@ def load_large_json(file_path):
 
 # Mapeamento binário para valores categóricos
 binary_mappings = {
-    "genero": {"MASCULINO": "00", "FEMININO": "01", "OUTRO": "10", "DESCONHECIDO": "11"},
+    "genero": {"MASCULINO": "0", "FEMININO": "1"},
     "nacionalidade": {"BRASILEIRA": "0", "ESTRANGEIRA": "1"},
-    "estado_civil": {"SOLTEIRO": "000", "CASADO": "001", "SEPARADO": "010", "VIUVO": "011", "DIVORCIADO": "100", "DESCONHECIDO": "101"},
+    "estado_civil": {"SOLTEIRO": "000", "CASADO": "001",  "VIÚVO": "010", "DIVORCIADO": "011", "-": "100"},
     "situacao": {"GRADUADO": "00", "ATIVO": "01", "INATIVO": "10"},
     "motivo_de_evasao": {"CANCELAMENTO POR ABANDONO": "0000", "REGULAR": "0001", "NAO COMPARECEU CADASTRO": "0010", "CANCELADO REPROVOU TODAS POR FALTAS": "0011", "CANCELADO NOVO INGRESSO OUTRO CURSO": "0100", "CANCELADO NOVO INGRESSO MESMO CURSO": "1111"},
-    "politica_afirmativa": {"A0": "0000", "L1": "0001", "L2": "0010", "L5": "0011", "L6": "0100", "L9": "0101", "L10": "0110", "L13": "0111", "L14": "1000", "BONUS": "1001"},
-    "tipo_de_ensino_medio": {"PRIVADA": "000", "PUBLICA": "001", "MAJORITARIAMENTE_PUBLICA": "010", "MAJORITARIAMENTE_PRIVADA": "011", "DESCONHECIDA": "100"},
-    "cor": {"BRANCA": "001", "PRETA": "010", "PARDA": "10", "INDÍGENA": "011", "MESTIÇA": "100", "ORIENTAL": "101", "OUTRAS": "110"},
-    "prac_deficiente": {"-": "0", "Sim": "1"},
-    "turno_do_curso":{"Integral":'11',"Matutino":'01',"Noturno":"10"}
+    "politica_afirmativa": {"BON. ESTADUAL": "00000", "L1": "00001", "L2": "00010", "L5": "00011", "L6": "00100", "L9": "00101", "L10": "00110", "L13": "00111", "L14": "01000", "-": "01001", "LI_PPI": "01010", "LI_PCD": "01011", "LI_EP": "01100", "LB_EP": "01101", "LB_Q": "01110", "LB_PPI": "01111", "LB_PCD": "10001"},
+    "tipo_de_ensino_medio": {"SOMENTE ESCOLA PRIVADA": "000", "SOMENTE ESCOLA PÚBLICA": "001", "PÚBLICA E PRIVADA, TENDO FICADO MAIS TEMPO EM ESCOLA PÚBLICA": "010", "PÚBLICA E PRIVADA, TENDO FICADO MAIS TEMPO EM ESCOLA PRIVADA": "011", "-": "100"},
+    "cor": {"BRANCA": "000", "PRETA": "001", "PARDA": "010", "AMARELA": "011", "NÃO DECLARADA": "100", "-": "101"},
+    "prac_deficiente": {"NÃO": "00", "SIM": "01", "-": "10"},
+    "turno_do_curso":{"INTEGRAL":'11', "MATUTINO":'01', "NOTURNO":"10"}
 }
 
 #Ajuste do tipo_de_ensino_medio desconhecido
@@ -89,6 +89,12 @@ def getInputOutput():
     #Fazer lista de turnos
     dataframe["tipo_de_ensino_medio"] = dataframe.apply(adjust_secondary_school_type, axis=1)
 
+    #Transformando todos os inputs em maiúsculo para padronizar o mapeamento
+    dataframe = dataframe.applymap(lambda x: x.upper() if isinstance(x, str) else x)
+
+    #Transformando todas as idades em inteiro antes de realizar o mapeamento
+    dataframe['idade'] = dataframe['idade'].astype(int)
+
     # Converter valores categóricos para sua representação binária
     for column, mapping in binary_mappings.items():
         dataframe[column] = dataframe[column].map(mapping)
@@ -125,7 +131,7 @@ def getInputOutput():
     evaded_list = dataframe_balanced['evaded'].tolist()
 
     # Colunas a serem mantidas
-    columns_to_keep = ["idade", "genero", "nacionalidade", "estado_civil", "politica_afirmativa", "tipo_de_ensino_medio", "turno", "cor", "prac_renda_per_capita_ate", "prac_deficiente"]
+    columns_to_keep = ["idade", "genero", "nacionalidade", "estado_civil", "politica_afirmativa", "tipo_de_ensino_medio", "turno_do_curso", "cor", "prac_renda_per_capita_ate", "prac_deficiente"]
 
     # Remover todas as colunas que não estão em columns_to_keep
     # Dataframe se torna dataframe_balanced
@@ -138,6 +144,7 @@ def getInputOutput():
     #Concatenar os resultados em uma string binária por linha
     binary_strings = dataframe.apply(lambda row: "".join(row.values), axis=1).to_list()
     #print(dataframe)
+    print(binary_strings)
     return(binary_strings,evaded_list)
 
 getInputOutput()
