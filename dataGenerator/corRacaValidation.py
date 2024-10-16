672eeb39a5c90ab33f6a486bfb603ba6022d8896
GuilhermeFernandesCC <guilherme.fernandes@ccc.ufcg.edu.br>
2024-10-16 17:05:41 -0300

    taxaCorPrac

--- dataGenerator/corRacaValidation.py
+++ dataGenerator/corRacaValidation.py
@@ -4,6 +4,19 @@ def load_large_json(file_path):
 # Caminho para o arquivo JSON
 file_path = r'data/students.json'
 dataframe = load_large_json(file_path)
+prac_count = 0
+erro = 0
 
-for aluno in dataframe:
-    print(aluno[0])
+for i in range(len(dataframe)):
+    aluno = dataframe.iloc[i]
+    if aluno["prac_cor"]!="-":
+        print( aluno["prac_cor"])
+        print(aluno["cor"])
+        prac_count+=1
+        
+        if aluno["prac_cor"].upper() != aluno["cor"].upper():
+            erro+=1
+print("#")
+print(f"Com registro de cor Prac:  {prac_count}")
+print(f"Com registro de cor que difere do registro Prac:  {erro}")
+print(f"Taxa de irregularidade: {(erro/prac_count)*100:.2f}%")
