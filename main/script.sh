9f16c32a86e2254a1814b49a072c6609f325505b
GuilhermeFernandesCC <guilherme.fernandes@ccc.ufcg.edu.br>
2024-11-09 11:19:16 -0300

    add: script.sh commit 1

---
+++ main/script.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+#Script Inicializador
+
+#Credentials
+echo 'Login Scao:'
+read login
+
+echo 'Senha:'
+read -s senha
+
+caminho_arquivo="../data/credentials.json"
+mkdir -p "$(dirname "$caminho_arquivo")"
+
+echo "{
+  \"credentials\": {
+    \"username\": \"$login\",
+    \"password\": \"$senha\"
+  }
+}" > "$caminho_arquivo"
+
+
+# Verifica se python3 está disponível
+if command -v python3 &>/dev/null; then
+	python_cmd="python3"
+elif command -v python &>/dev/null; then
+	python_cmd="python"
+else
+	echo "Erro: Nenhuma versão do Python foi encontrada"
+	exit 1
+fi
+
+$python_cmd main.py
+
+if [ ! -f ../requirements.txt ]; then
+    echo "Arquivo requirements.txt não encontrado!"
+    exit 1
+fi
+
+echo "Conteúdo do requirements.txt:"
+cat requirements.txt
+read -p "Deseja instalar os módulos listados em requirements.txt ? (s/n)" resposta
+#converter em minúsculo
+resposta = ${resposta,,}
+
+if [[ "$resposta" == "s" || "$resposta" == "sim" || "$resposta" == "y" || "$resposta" == "yes" ]]; then
+	# Execurtar o comando de instalação
+	echo "Instalando os Módulos ..."
+	pip install requirements.txt
+	echo "Instalação concluída!"
+else 
+	echo "Instalação cancelada pelo usuário."
+	exit 1
+fi
+# gera estudantes para treino
+$python_cmd ../dataGenerator/genStudents.py 
+
+# estudantes para prever TO DO
+$python_cmd ../dataGenerator/getStudent.py $1
+
+# regressão logistica
+$python_cmd ../data/dataGenerator/logisticRegression.py
+
+echo "Recuperando estudantes"
