37b27908b41dcdba54d90f17b1cb9c506781a839
GuilhermeFernandesCC <guilherme.fernandes@ccc.ufcg.edu.br>
2024-12-27 19:17:31 -0300

    Fix: encoded X_prev

--- dataGenerator/regressaoLogistica.py
+++ dataGenerator/regressaoLogistica.py
@@ -4,6 +4,8 @@ import numpy as np
 import pandas as pd
 ##Import dados processados
 import sys
+
+from sklearn.preprocessing import OneHotEncoder
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../processing')))
 
 #sys.path.insert(1, r'./processingprocessingPrevisao')
@@ -26,9 +28,6 @@ from sklearn.metrics import accuracy_score
 ## Data treinamento 
 X_train, Y_train, X_test, Y_test,dataframeTreinamento,feature_cols = processing.getInputOutput(undersampling=False,regressao=True)
 logreg = LogisticRegression(random_state=16,max_iter=100000)
-print(X_train)
-print(Y_train)
-
 
 #Garantir que motivo de evasão não é passado
 for col in ['motivo_de_evasao']:
@@ -36,22 +35,38 @@ for col in ['motivo_de_evasao']:
         X_train = X_train.drop(columns=[col])
     if col in X_test.columns:
         X_test = X_test.drop(columns=[col])
-logreg.fit(X_train, Y_train)
 
-Y_pred_test = logreg.predict(X_test)
+# Usar One-Hot Encoding para lidar com variáveis categóricas
+encoder = OneHotEncoder(drop='first', sparse_output=False, handle_unknown='ignore')  # Ignorar categorias desconhecidas
+X_train_encoded = encoder.fit_transform(X_train)
+X_test_encoded = encoder.transform(X_test)
+# Obter os nomes das features após a codificação
+encoded_feature_names = encoder.get_feature_names_out()
+# Treinar o modelo de Regressão Logística
+logreg = LogisticRegression(random_state=16, max_iter=100000)
+logreg.fit(X_train_encoded, Y_train)
 
+
+# Previsão dos teste
+Y_pred_test = logreg.predict(X_test_encoded)
+# Verificação da Acurácia do teste
 acuracia = accuracy_score(Y_test, Y_pred_test)
 print("Acurácia: ",acuracia)
 
 #Dados a terem suas evasões previstas 
 file_path = r'../data/studentsPrediction.json'
-
+#
 X_prev,dataframeCopia,columns_to_keep = processingPrevisao.studentsDataframe(file_path)
-
+#
 matriculas = X_prev['matricula_do_estudante']
 X_prev = X_prev.drop(columns='matricula_do_estudante')
+#
+X_prev_encoded = encoder.transform(X_prev)
+#
+encoded_feature_names = encoder.get_feature_names_out()
 
-Y_prev = logreg.predict(X_prev)
+
+Y_prev = logreg.predict(X_prev_encoded)
 
 # Mapeando os valores para as strings desejadas
 Y_prev_mapped = ['Sem risco' if y == '0' else 'Com risco' for y in Y_prev]
